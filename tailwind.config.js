4229b54a83c4261466654f54d894687dd65bd76e
F776 <fern.belmar@duocuc.cl>
2024-03-27 16:36:25 -0300

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  mode: 'jit',
   content: ["**/*.{html, js}", "**/**/*.{html, js}", "./index.html"],
   theme: {
     screens: {
@@ -9,7 +10,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        'primary-color': '#0fa'
+        'primary-color': '#0fa',
+        'secondary-color': '#f0f',
       }
     },
   },
